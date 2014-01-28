3b700ad314f58f66f4d7ecbd5de84e31c48ac319
aviflombaum <avi@flombaum.com>
2014-01-28 14:38:05 -0500

    initial lab

---
+++ README.md
@@ -0,0 +1,8 @@
+---
+language: ruby
+tags: methods, arguments, variables, default arguments
+---
+
+# Say Hello
+
+Your goal is to build a method that says hello to people. Look at say_hello_spec.rb and program your solution in say_hello.rb.
---
+++ spec/say_hello_spec.rb
@@ -0,0 +1,15 @@
+require_relative './spec_helper'
+require_relative '../say_hello'
+
+describe "say_hello" do 
+  
+  it 'accepts an argument of a name and prints out Hello with that Name' do
+    expect($stdout).to receive(:puts).with("Hello Kent Beck!")
+    say_hello("Kent Beck")
+  end
+
+  it 'defaults to your name Ruby Programmer when no name is passed in' do
+    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+    say_hello()
+  end
+end
