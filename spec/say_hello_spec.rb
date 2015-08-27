590c72608ee95d85ee176bb2ed639328fb567ded
aviflombaum <avi@flombaum.com>
2015-08-27 07:06:41 -0400

    made default value for name argument more explicit in spec

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -7,7 +7,7 @@ describe "say_hello" do
     say_hello("Kent Beck")
   end
 
-  it 'defaults to your name Ruby Programmer when no name is passed in' do
+  it 'defaults to Ruby Programmer when no name is passed in' do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()
   end
