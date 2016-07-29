f3a820678fc98f68c114fa6ea1635d2fc17f7710
Christina Anglin <anglin.christina@gmail.com>
2016-07-29 20:24:18 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,17 @@
 # Build your say_hello method here
+
+=begin #This is the real program
+def say_hello(name = "Ruby Programmer")
+  puts "What is your name?"
+  name = gets
+  print "Hello #{name}!"
+end
+=end
+
+#This is what this lab requires
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+say_hello("Christina")
+say_hello()
