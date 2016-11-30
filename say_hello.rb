d214a44a4605b6c7e65bf8374a62cd35c772c78b
Kira Rice <kiramrice@gmail.com>
2016-11-30 15:54:52 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
-# Build your say_hello method here
+#def say_hello("Kira")
+#end
+
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting
+
+greeting("Kira")
