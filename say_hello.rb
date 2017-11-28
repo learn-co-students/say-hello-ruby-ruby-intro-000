38cf8e62b805770813ababd292d5d47fce481db6
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-28 16:28:47 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
 # Build your say_hello method here
+def say_hello(name, language = "Ruby Programmer")
+  if name == ""
+    puts "Hello #{language}!"
+  end
+  else
+    puts "Hello #{name}!"
+  end
+end
