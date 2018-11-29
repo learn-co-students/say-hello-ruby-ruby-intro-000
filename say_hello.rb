7f62c2cbf4bc1d9320b67c213563ff891c5f21ee
shivani brijmohan <sbrij001@yahoo.com>
2018-11-29 18:41:45 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,9 @@
+#This method should accept the argument of a person's name.
+#It then should print `"Hello "` with the name followed by an exclamation point
+#(don't forget that space after the "Hello").
+##say_hello should have a default argument of "Ruby Programmer"
 # Build your say_hello method here
+
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
