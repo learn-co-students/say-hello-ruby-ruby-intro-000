d60e6487528fc15897977ad252ed621a5ec18543
Nixson Henry <henrynixon.hn@gmail.com>
2019-02-08 16:04:45 +0000

    Done.

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,7 +1,12 @@
+def say_hello(name)
+  puts "Hello #{name}"
+  "Hello Ruby Programmer!"
+end
+
 require_relative './spec_helper'
 
-describe "say_hello" do 
-  
+describe "say_hello" do
+
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
