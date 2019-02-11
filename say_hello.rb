94f5b086ea12ac08f6ef59e75e26afc57d935da2
Sridevi Patchipulusu <sridevips@hotmail.com>
2019-02-11 00:30:04 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
 # Build your say_hello method here
+say_hello(name){
+puts "Hello with that #{name}"
+}
+say_hello("Kent Back")
+
+say_hello(name = "Ruby Programmer"){
+puts "Hello, #{name}"
+}
+say_hello()
