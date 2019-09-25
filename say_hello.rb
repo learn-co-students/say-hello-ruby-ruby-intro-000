713162abd47fce3a68dee3d2396cd4bef098e0c9
Philip Lee-Chin <phil.leechin@outlook.com>
2019-09-25 01:09:01 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,6 @@
 # Build your say_hello method here
-say_hello("Philip")
+def say_hello(name = "Ruby Programmer")
 
 # The method prints this text to the screen:
-Hello Philip!
+  puts "Hello #{name}!"
+end
