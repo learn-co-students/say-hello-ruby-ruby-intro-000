756b10b05ce31d92ba4882f399db4057dd0c4b3e
Salvador Castaneda <salcasta@gmail.com>
2019-11-12 00:33:15 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
-# Build your say_hello method here
+# Build your say_hello method h
+
+def say_hello("Kent Beck")
+  puts "Hello Kent Beck!"
+end
+
+def say_hello(name = "Ruby Programmer!")
+  puts "Hello #{name}"
+end
