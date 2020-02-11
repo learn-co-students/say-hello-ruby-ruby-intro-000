5657a6bb6ffe8a27f36dfb73a74332f343554457
Michael Schneider <mic.sch91@gmail.com>
2020-02-11 21:48:04 +0000

    Done.

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,10 +1,10 @@
 require_relative './spec_helper'
 
-describe "say_hello" do 
-  
+describe "say_hello" do
+
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
-    say_hello("Kent Beck")
+    say_hello("Kent Beck!")
   end
 
   it 'defaults to Ruby Programmer when no name is passed in' do
