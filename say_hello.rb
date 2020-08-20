dc964928dd5e64b045b54f1f81c37a3f1e133cdf
Charles Lai <charlesangeloplai@gmail.com>
2020-08-20 19:20:44 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,13 @@
 # Build your say_hello method here
+## Steps
+
+# 1. Run `learn` to see where you stand.
+#  * You probably got two NoMethodErrors. This means the test was looking for a method called `say_hello` but couldn't find it.
+# 2. Define the method in `say_hello.rb`
+#  * Run `learn` again. Are you getting a different error? If you haven't seen this error, guess what it's trying to tell you then Google it with the word "ruby".
+# 3. Get all the tests to pass!
+# 4. Once all of the tests are passing, use `learn submit` to submit your lab.
+
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
