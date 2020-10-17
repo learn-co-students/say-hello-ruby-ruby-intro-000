7c21fbbb64ca5596cfbeef764752b515c07edd36
poignant-semicolon-2878 <jndossantos00@gmail.com>
2020-10-17 22:41:41 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
-def greeting='say_hello,Gabriela'
-  puts Hello!
+def say_hello(name="Gabriela")
+  puts "Hello #{name}!"
+end
+
+
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
 end
